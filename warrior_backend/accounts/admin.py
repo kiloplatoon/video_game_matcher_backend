1d40998778e6f83d473ac6c4c785340719813806
vrgabrieljr <gabrielvrcode@gmail.com>
2020-04-06 17:51:46 -0500

    Created Django App accounts. Created Custom user model with authtoken

---
+++ warrior_backend/accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.apps import AppConfig
+# Register your models here.
+
+class AuthConfig(AppConfig):
+    name = 'accounts'
---
+++ warrior_backend/accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    
+    USERNAME_FIELD = 'email'
+
+    def get_username(self):
+        return self.email
