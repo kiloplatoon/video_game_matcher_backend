7b4d3bc2a66bfa5cb132c54a2fa579088e094fde
lubayb1 <bryanlubay1@gmail.com>
2020-04-09 12:16:55 -0700

    friendships backend ready for QA testing

--- warrior_backend/friendships/urls.py
+++ warrior_backend/friendships/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('<int:user_id>/delete', views.delete_user, name='delete_user'), # probably can delete
 
     path('relationship', views.relationship_detail, name='relationship_detail'),
-    path('relationship/friend_request', views.friend_request, name='friend_request'), # need to fix both ways
+    path('relationship/friend_request', views.friend_request, name='friend_request'),
 
     #                  action user = who is accepting           user_id = who is being accepted by action user
     path('relationship/<int:action_user>/accept_friend_request/<int:user_id>', views.accept_friend_request, name='accept_friend_request'),
 
-    path('relationship/<int:action_user>/check_friendship/<int:user_id>', views.check_friendship, name='check_friendship'), # fix this
-    path('relationship/<int:action_user>/friends_list', views.friends_list, name='friends_list'), # fix this
+    path('relationship/<int:action_user>/check_friendship/<int:user_id>', views.check_friendship, name='check_friendship'), 
+    path('relationship/<int:action_user>/friends_list', views.friends_list, name='friends_list'),
     path('relationship/<int:action_user>/pending_requests', views.pending_requests, name='pending_requests'),
     path('relationship/<int:action_user>/friend_requests_sent_by_user', views.friend_requests_sent_by_user, name='friend_requests_sent_by_user'),
 
--- warrior_backend/friendships/views.py
+++ warrior_backend/friendships/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
 
 from .forms import RelationshipForm
 from .models import Relationship
@@ -58,10 +59,13 @@ def friend_request(request):
     if request.method == 'POST':
         form = RelationshipForm(request.POST)
         if form.is_valid():
+            print("WTF")
             relationship = form.save(commit=True)
             serialized_relationship = RelationshipSerializer(relationship).relationship_detail            
             return JsonResponse(data={'Success': 'You have created a new relationship!', 'relationship': serialized_relationship}, status=200)
-
+        else:
+            return JsonResponse(data={'Error': 'Relationship already exists or form not valid!'}, status=200)
+            
 @csrf_exempt
 def accept_friend_request(request, action_user, user_id):
     Relationship.objects.filter(user_one=action_user, user_two=user_id).update(status=1, action_user=action_user)# .values()
@@ -73,15 +77,21 @@ def accept_friend_request(request, action_user, user_id):
 # 0 = Pending 1 = Accepted 2 = Declined 3 = Blocked
 @csrf_exempt
 def check_friendship(request, action_user, user_id):
-    temp = Relationship.objects.filter(user_one=action_user, user_two=user_id).values()
+    temp = Relationship.objects.filter(Q(user_one=action_user, user_two=user_id) | Q(user_two=action_user, user_one=user_id)).values()
     user1 = User.objects.get(id=action_user)
     user2 = User.objects.get(id=user_id)
+
     if len(temp) == 0:
         return JsonResponse(data={f'Success!': f'No relation found between {user1.username} and {user2.username}!'})
 
     status = temp[0]['status']
-    if (status == 0):
+    action_user_Q = temp[0]['action_user_id']
+    if (status == 0 and action_user == action_user_Q):
         return JsonResponse(data={f'Success!': f'{user1.username} has a pending friend request to {user2.username}.'})
+    elif (status == 0 and user_id == action_user_Q):
+        return JsonResponse(data={f'Success!': f'{user1.username} has a pending friend request from {user2.username}.'})
+
+
     elif (status == 1):
         return JsonResponse(data={f'Success!': f'{user1.username} and {user2.username} are friends.'})
     elif (status == 2):
@@ -91,11 +101,17 @@ def check_friendship(request, action_user, user_id):
 
 @csrf_exempt
 def friends_list(request, action_user):
-    temp = Relationship.objects.filter(user_one=action_user, status=1).values()
-
+    temp = Relationship.objects.filter(Q(user_one=action_user, status=1) | Q(user_two=action_user, status=1) ).values()
+    print(len(temp))
+    print(temp)
     list = []
+    user1 = User.objects.get(id=action_user)
     for i in temp:
-        list.append(User.objects.get(id = i['user_two_id']).username)
+        if user1.username != User.objects.get(id = i['user_two_id']).username:
+            list.append(User.objects.get(id = i['user_two_id']).username)
+
+        if user1.username != User.objects.get(id = i['user_one_id']).username:
+            list.append(User.objects.get(id = i['user_one_id']).username)
 
     user1 = User.objects.get(id=action_user)
     return JsonResponse(data={f'Success!': f'{user1.username} friend list: {list}'})
